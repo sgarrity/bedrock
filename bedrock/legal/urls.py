16f975668f04e913062635695dbf00c31f3421de
Steven Garrity <steven@silverorange.com>
2014-08-21 20:21:59 -0300

    Partial start of moving foundation licensing pages to legal section

--- bedrock/legal/urls.py
+++ bedrock/legal/urls.py
@@ -9,11 +9,21 @@ from bedrock.legal import views
 
 urlpatterns = patterns('',
     page('', 'legal/index.html'),
+
     page('eula', 'legal/eula.html'),
     page('eula/firefox-2', 'legal/eula/firefox-2-eula.html'),
     page('eula/firefox-3', 'legal/eula/firefox-3-eula.html'),
+
     page('firefox', 'legal/firefox.html'),
+
     page('terms/mozilla', 'legal/terms/mozilla.html'),
     page('terms/persona', 'legal/terms/persona.html'),
+
+    page('licensing', 'legal/licensing.html'),
+    page('licensing/website-content', 'legal/licensing/website-content.html'),
+    page('licensing/website-markup', 'legal/licensing/website-markup.html'),
+    page('licensing/binary-components', 'legal/licensing/binary-components.html'),
+    page('licensing/binary-components/rationale', 'legal/licensing/binary-components-rationale.html'),
+
     url('^fraud-report/$', views.fraud_report, name='legal.fraud-report'),
 )
