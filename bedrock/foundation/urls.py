6b681ee37973927be3648d8c091818678238acd4
Steven Garrity <steven@silverorange.com>
2014-08-26 15:57:15 -0300

    Move trademarks & licensing pages under Legal hub (Bug 997577)

--- bedrock/foundation/urls.py
+++ bedrock/foundation/urls.py
@@ -57,16 +57,6 @@ urlpatterns = patterns('',
     page('openwebfund/more', 'foundation/openwebfund/more.html'),
     page('openwebfund/thanks', 'foundation/openwebfund/thanks.html'),
 
-    page('trademarks', 'foundation/trademarks/index.html'),
-    page('trademarks/policy', 'foundation/trademarks/policy.html'),
-    page('trademarks/list', 'foundation/trademarks/list.html'),
-    page('trademarks/faq', 'foundation/trademarks/faq.html'),
-    page('trademarks/l10n-website-policy', 'foundation/trademarks/l10n-website-policy.html'),
-    page('trademarks/distribution-policy', 'foundation/trademarks/distribution-policy.html'),
-    page('trademarks/community-edition-permitted-changes', 'foundation/trademarks/community-edition-permitted-changes.html'),
-    page('trademarks/community-edition-policy', 'foundation/trademarks/community-edition-policy.html'),
-    page('trademarks/poweredby/faq', 'foundation/trademarks/poweredby/faq.html'),
-
     # documents
     page('documents', 'foundation/documents/index.html'),
 )
