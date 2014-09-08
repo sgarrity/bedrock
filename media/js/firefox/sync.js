b19cb0b4464cfd30b31780520c2bd3f3eed7c211
Steven Garrity <steven@silverorange.com>
2014-09-08 20:41:38 -0300

    Add GA tracking to Firefox Sync page Bug 1058830

--- media/js/firefox/sync.js
+++ media/js/firefox/sync.js
@@ -20,6 +20,8 @@
     // Default (do nothing)
 
     var fxMasterVersion = window.getFirefoxMasterVersion();
+    var state = 'Unknown';
+
 
     // Variations 1-5 are Firefox
     if (window.isFirefox()) {
@@ -28,6 +30,8 @@
         if (window.isFirefoxMobile()) {
 
             $('body').addClass('state-fx-android');
+            state = 'Firefox for Android';
+
 
         // Variation #1-4: Firefox for Desktop
         } else {
@@ -41,10 +45,12 @@
                     if (config.setup) {
 
                         $('body').addClass('state-fx-31-signed-in');
+                        state = 'Latest Version of Firefox or Higher: Signed-In';
 
                     // Variation #2: Firefox 31+ signed OUT of Sync
                     } else {
                         $('body').addClass('state-fx-31-signed-out');
+                        state = 'Latest Version of Firefox or Higher: Signed-Out';
                     }
 
                 });
@@ -52,10 +58,12 @@
             // Variation #3: Firefox 29 or 30
             } else if (fxMasterVersion === 29 || fxMasterVersion === 30) {
                 $('body').addClass('state-fx-29-30');
+                state = 'Previous 2 Versions of Firefox';
 
             // Variation #4: Firefox 28 or older
             } else if (fxMasterVersion <= 28) {
                 $('body').addClass('state-fx-28-older');
+                state = 'Older Version of Firefox';
             }
 
         }
@@ -63,6 +71,31 @@
     // Variation #6: Not Firefox
     } else {
         $('body').addClass('state-not-fx');
+        state = 'Not Firefox';
     }
+
+    // Send page state to GA
+    _gaq.push(['_trackEvent', '/sync/ Page Interactions', 'load', state]);
+
+    // Setup GA tracking for Firefox download button
+    $('#cta-firefox').on('click', function() {
+        _gaq.push(['_trackEvent', 'Firefox Downloads', 'download click', 'Firefox']);
+    });
+
+    // Setup GA tracking for Firefox for primary Android download button
+    $('#cta-android').on('click', function() {
+        _gaq.push(['_trackEvent', 'Firefox Downloads', 'top', 'Firefox for Android']);
+    });
+
+    // Setup GA tracking for Firefox for Android footer download button
+    $('#cta-android-footer').on('click', function() {
+        _gaq.push(['_trackEvent', 'Firefox Downloads', 'bottom', 'Firefox for Android']);
+    });
+
+    // Setup GA tracking for Sync button
+    $('#cta-sync').on('click', function() {
+        _gaq.push(['_trackEvent', '/sync/ Page Interactions', 'button click', 'Sync CTA']);
+    });
+
 
 })(window.jQuery);
